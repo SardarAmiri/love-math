db3947ef707364506a6e7ca6ac26b8128550b6f6
Sardar Amiri <sardaramiri1122@gmail.com>
2024-12-06 15:48:46 +0000

    create required funcion

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function runGame(){
+
+}
+function checkAnswer(){
+    
+}
+function calculateCorrectAnswer(){
+    
+}
+function increamentScore(){
+    
+}
+function increatmentWrongAnswer(){
+    
+}
+function displayAdditionQuestion(){
+    
+}
+function displaySubtractQuestion(){
+    
+}
+function displayMultiplyQuestion(){
+    
+}
