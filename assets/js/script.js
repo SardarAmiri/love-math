2cea33a8c480fd436ea09cfef09dbf8470cdc5ea
Sardar Amiri <sardaramiri1122@gmail.com>
2024-12-06 16:36:50 +0000

    add addtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,25 @@ document.addEventListener('DOMContentLoaded', function () {
                 alert('you clicked submit')
             }else {
                 let gameType = this.getAttribute('data-type')
-                alert(`you clicked ${gameType}`)
+                runGame(gameType)
             }
         })
     }
+    runGame('addition')
 })
 /**
  * the main game "loop" called when the script is first loaded,
  * and after the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){
     let num1 = Math.floor(Math.random() * 25 + 1)
     let num2 = Math.floor(Math.random() * 25 + 1)
+    if(gameType === 'addition'){
+        displayAdditionQuestion(num1, num2)
+    }else {
+        alert(`Error: unknown game type ${gameType}`)
+        throw('Unknown game type')
+    }
 
 }
 function checkAnswer(){
@@ -32,7 +39,10 @@ function increamentScore(){
 function increatmentWrongAnswer(){
     
 }
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').innerText = operand1
+    document.getElementById('operand2').innerText = operand2
+    document.getElementById('operator').innerText = '+'
     
 }
 function displaySubtractQuestion(){
