862bfb09bdb723cdd56292ee302729b7ec63a8ae
Sardar Amiri <sardaramiri1122@gmail.com>
2024-12-09 11:45:54 +0000

    check the user answer with correct answer and increatment or decreament with respect the the iscorrect answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     for(let button of buttons){
         button.addEventListener('click', function(){
             if(this.getAttribute('data-type') === 'submit'){
-                alert('you clicked submit')
+                checkAnswer()
             }else {
                 let gameType = this.getAttribute('data-type')
                 runGame(gameType)
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function runGame(gameType){
     let num1 = Math.floor(Math.random() * 25 + 1)
     let num2 = Math.floor(Math.random() * 25 + 1)
-    
+
     if(gameType === 'addition'){
         displayAdditionQuestion(num1, num2)
     }else if(gameType === 'subtract'){
@@ -36,16 +36,53 @@ function runGame(gameType){
     }
 
 }
+
+/**
+ * Check user answer and the correct answer return from calculateCorrectAnswer function
+ */
 function checkAnswer(){
+    const userAnswer = parseInt(document.getElementById('answer-box').value)
+    const correctAnswer = calculateCorrectAnswer()
+    const isCorrect = userAnswer === correctAnswer[0]
+    if(isCorrect){
+        increamentScore()
+    }else {
+        increamentScore()
+    }
+    runGame(correctAnswer[1])
     
 }
+/**
+ * 
+ * get operands and operator directly from dom and return an array with correct answer and addition
+ */
 function calculateCorrectAnswer(){
+    let operand1 = parseInt(document.getElementById('operand1').innerText)
+    let operand2 = parseInt(document.getElementById('operand2').innerText)
+    let operator = document.getElementById('operator').innerText
+    if(operator === '+'){
+        return [operand1 + operand2, 'addition']
+    }else {
+        alert('unimplemented operator', operator)
+        throw `unimplemented operator ${operator}`
+    }
     
 }
+
+/**
+ * get the currrent socre from the dom and then increament by 1
+ */
 function increamentScore(){
-    
+    let score = parseInt(document.getElementById('score').textContent)
+    document.getElementById('score').innerText = ++score
 }
+/**
+ * get the current incorrect from the DOM and then decreament by 1
+ */
 function increatmentWrongAnswer(){
+    let score = parseInt(document.getElementById('incorrect').textContent)
+    document.getElementById('incorrect').innerText = ++score
+    
     
 }
 function displayAdditionQuestion(operand1, operand2){
